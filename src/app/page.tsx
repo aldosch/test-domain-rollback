de800aa51850bf839411987ed15bb0f7c8f0e587
Aldo <git@aldo.io>
2023-11-29 15:32:11 +1100

    deployment 004

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ export default function Home() {
     <main className="bg-violet-100 flex items-center justify-center h-screen">
       <div className="px-4 bg-white rounded-lg shadow">
         <div className="sm:p-6 px-4 py-5">
-          <h1 className="font-mono text-2xl font-semibold">Deployment 003</h1>
+          <h1 className="font-mono text-2xl font-semibold">Deployment 004</h1>
         </div>
       </div>
     </main>
