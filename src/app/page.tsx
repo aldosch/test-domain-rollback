45fca520e46d4a8414f7d5b5d023cbaf2d29ddbb
Aldo <git@aldo.io>
2023-11-29 15:26:09 +1100

    deployment 002

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
     <main className="bg-violet-100 flex items-center justify-center h-screen">
       <div className="px-4 bg-white rounded-lg shadow">
         <div className="sm:p-6 px-4 py-5">
-          <h1 className="font-mono text-2xl font-semibold">Deployment 001</h1>
+          <h1 className="font-mono text-2xl font-semibold">Deployment 002</h1>
         </div>
       </div>
     </main>
